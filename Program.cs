d2b0bb430f3fc870f608b26f6c3b26b7fd4839dc
Novák Gábor <novakg17@gmail.com>
2023-06-29 11:47:49 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using Json.More;
+using Json.Schema;
+using System.Text.Json;
+using System.Text;
+using System.Text.Json.Nodes;
+using System.Text.Encodings.Web;
+using System.Text.Unicode;
+
+namespace json_everythingValidationTest
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            //Build Schema Registry
+            string[] files = Directory.GetFiles("files/schemas/", "*.json", SearchOption.AllDirectories);
+
+            foreach (var file in files)
+            {
+                var path = Path.GetFullPath(file);
+                var schema = JsonSchema.FromFile(file);
+
+                SchemaRegistry.Global.Register(new Uri(path), schema);
+            }
+
+            await ValidateFileAsync();
+
+            Console.ReadKey();
+        }
+
+        private static async Task ValidateFileAsync()
+        {
+            var jsonText = await File.ReadAllTextAsync("files/input/test.json");
+            var json = JsonNode.Parse(jsonText);
+
+            var schema = JsonSchema.FromFile("files/schemas/main.schema.json");
+
+            //Evaluation
+            EvaluationResults? result = schema.Evaluate(json);
+
+            //Printing out the result:
+            var formattedResult = result.ToJsonDocument();
+            using (var stream = new MemoryStream())
+            {
+                Utf8JsonWriter writer = new Utf8JsonWriter(stream, new JsonWriterOptions { Indented = true });
+                formattedResult.WriteTo(writer);
+                writer.Flush(); 
+                string output = Encoding.UTF8.GetString(stream.ToArray());
+                Console.WriteLine(output);
+            }
+        }
+    }
+}
